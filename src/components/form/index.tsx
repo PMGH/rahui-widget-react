f3506447aa6f68aadacc8a3857d36c52864aef36
PMGH <petermccready2@gmail.com>
2024-08-22 22:50:32 +0100

    use react-datepicker

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 import { formattedTodayDate, timeOptionsAsHtml } from "../../helpers";
 import Select from "./Select";
 
+import "react-datepicker/dist/react-datepicker.css";
 import "./form.scss";
 
 type FormProps = {
@@ -18,186 +21,236 @@ const Form = ({
   buttonText = "Book",
   formClass = "",
   datePickerHiddenInputId = "hidden-datepicker-input-id",
-  datePickerId = "datepicker-id",
   heading = "Reserve a table",
   maxCoversPerBooking,
   timePickerId = "timepicker-id",
   widgetPreview = false,
-}: FormProps) => (
-  <section id="rahui-widget" className="widget__container">
-    <header className="widget__header">
-      <h3>{heading}</h3>
-    </header>
+}: FormProps) => {
+  const [startDate, setStartDate] = useState(new Date());
 
-    <div id="error-message">Sorry, something went wrong. Please try again.</div>
+  const isPastDate = (date: Date) => {
+    const now = new Date();
+    const comparableDatetimeNow = new Date(now.setHours(0, 0, 0));
+    const comparableDatetime = new Date(date.setHours(15, 0, 0));
+    return comparableDatetime.getTime() > comparableDatetimeNow.getTime();
+  };
 
-    <div id="confirmation-message-container">
-      <div className="wrapper">
-        <svg
-          className="checkmark"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 52 52"
-        >
-          {" "}
-          <circle
-            className="checkmark__circle"
-            cx="26"
-            cy="26"
-            r="25"
-            fill="none"
-          />{" "}
-          <path
-            className="checkmark__check"
-            fill="none"
-            d="M14.1 27.2l7.1 7.2 16.7-16.8"
-          />
-        </svg>
+  return (
+    <section id="rahui-widget" className="widget__container">
+      <header className="widget__header">
+        <h3>{heading}</h3>
+      </header>
+
+      <div id="error-message">
+        Sorry, something went wrong. Please try again.
       </div>
-      <h3 id="confirmation-message">Booking confirmed!</h3>
-      <div id="confirmation-booking-container">
-        <span id="confirmation-booking-number-of-covers"></span>
-        <span id="confirmation-booking-datetime"></span>
-      </div>
-    </div>
 
-    <form className={formClass}>
-      <input
-        type="hidden"
-        id="widget-submission"
-        name="widget-submission"
-        value="true"
-      ></input>
-      <div className="form__field__group">
-        <div className="form__field form-group-left">
-          <div className="form__field form__field__required">
-            <div className="form__field__required">
-              <label
-                id="number_of_covers_label"
-                htmlFor="booking[number_of_covers]"
-              >
-                Guests{" "}
-                {maxCoversPerBooking ? ` (Max: ${maxCoversPerBooking})` : ""}
-              </label>
-              <span className="required-field-symbol">*</span>
-            </div>
-            <input
-              type="number"
-              id="number_of_covers"
-              name="booking[number_of_covers]"
-              placeholder="1"
-              min="1"
-              value="2"
-              required
+      <div id="confirmation-message-container">
+        <div className="wrapper">
+          <svg
+            className="checkmark"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52"
+          >
+            {" "}
+            <circle
+              className="checkmark__circle"
+              cx="26"
+              cy="26"
+              r="25"
+              fill="none"
+            />{" "}
+            <path
+              className="checkmark__check"
+              fill="none"
+              d="M14.1 27.2l7.1 7.2 16.7-16.8"
             />
-          </div>
-          <div className="form__field__required">
-            <input
-              type="hidden"
-              id={datePickerHiddenInputId}
-              name="booking[date]"
-              required
-              value={formattedTodayDate}
-            ></input>
-          </div>
-          <div className="time-select-container">
-            <Select
-              id={timePickerId}
-              name="booking[time]"
-              required
-              className="time-select"
-              options={timeOptionsAsHtml({
-                openAt: 6,
-                closeAt: 20,
-              })}
-            ></Select>
-          </div>
+          </svg>
         </div>
+        <h3 id="confirmation-message">Booking confirmed!</h3>
+        <div id="confirmation-booking-container">
+          <span id="confirmation-booking-number-of-covers"></span>
+          <span id="confirmation-booking-datetime"></span>
+        </div>
+      </div>
 
-        <div className="form-group-right">
-          <section className="customer-details">
-            <div className="form__field__group">
-              <div className="form__field">
-                <div className="form__field__required">
-                  <label htmlFor="customer[first_name]">First name</label>
-                  <span
-                    className="required-field-symbol"
-                    id="customer_first_name_required_symbol"
-                  >
-                    *
-                  </span>
-                </div>
-                <input
-                  type="text"
-                  id="customer_first_name"
-                  name="customer[first_name]"
-                  placeholder="Enter your first name"
-                  required
-                />
-              </div>
-              <div className="form__field last-name">
-                <div className="form__field__required">
-                  <label htmlFor="customer[last_name]">Last name</label>
-                  <span
-                    className="required-field-symbol"
-                    id="customer_last_name_required_symbol"
-                  >
-                    *
-                  </span>
-                </div>
-                <input
-                  type="text"
-                  id="customer_last_name"
-                  name="customer[last_name]"
-                  placeholder="Enter your last name"
-                  required
-                />
-              </div>
-            </div>
-            <div className="form__field">
+      <form className={formClass}>
+        <input
+          type="hidden"
+          id="widget-submission"
+          name="widget-submission"
+          value="true"
+        ></input>
+        <div className="form__field__group">
+          <div className="form__field form-group-left">
+            <div className="form__field form__field__required">
               <div className="form__field__required">
-                <label htmlFor="customer[email]">Email</label>
+                <label
+                  id="number_of_covers_label"
+                  htmlFor="booking[number_of_covers]"
+                >
+                  Guests{" "}
+                  {maxCoversPerBooking ? ` (Max: ${maxCoversPerBooking})` : ""}
+                </label>
                 <span className="required-field-symbol">*</span>
               </div>
-              <p className="info muted">
-                We send the booking confirmation to this email address
-              </p>
               <input
-                type="email"
-                id="email"
-                name="customer[email]"
-                placeholder="Enter your email address"
+                type="number"
+                id="number_of_covers"
+                name="booking[number_of_covers]"
+                placeholder="1"
+                min="1"
+                value="2"
+                required
               />
             </div>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => date && setStartDate(date)}
+              dateFormat="dd/MM/yyyy"
+              filterDate={isPastDate}
+              showIcon
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 48 48"
+                >
+                  <mask id="ipSApplication0">
+                    <g
+                      fill="none"
+                      stroke="#fff"
+                      strokeLinejoin="round"
+                      strokeWidth="4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        d="M40.04 22v20h-32V22"
+                      ></path>
+                      <path
+                        fill="#fff"
+                        d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
+                      ></path>
+                    </g>
+                  </mask>
+                  <path
+                    fill="currentColor"
+                    d="M0 0h48v48H0z"
+                    mask="url(#ipSApplication0)"
+                  ></path>
+                </svg>
+              }
+            />
+            <div className="form__field__required">
+              <input
+                type="hidden"
+                id={datePickerHiddenInputId}
+                name="booking[date]"
+                required
+                value={formattedTodayDate}
+              ></input>
+            </div>
+            <div className="time-select-container">
+              <Select
+                id={timePickerId}
+                name="booking[time]"
+                required
+                className="time-select"
+                options={timeOptionsAsHtml({
+                  openAt: 6,
+                  closeAt: 20,
+                })}
+              ></Select>
+            </div>
+          </div>
+
+          <div className="form-group-right">
+            <section className="customer-details">
+              <div className="form__field__group">
+                <div className="form__field">
+                  <div className="form__field__required">
+                    <label htmlFor="customer[first_name]">First name</label>
+                    <span
+                      className="required-field-symbol"
+                      id="customer_first_name_required_symbol"
+                    >
+                      *
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    id="customer_first_name"
+                    name="customer[first_name]"
+                    placeholder="Enter your first name"
+                    required
+                  />
+                </div>
+                <div className="form__field last-name">
+                  <div className="form__field__required">
+                    <label htmlFor="customer[last_name]">Last name</label>
+                    <span
+                      className="required-field-symbol"
+                      id="customer_last_name_required_symbol"
+                    >
+                      *
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    id="customer_last_name"
+                    name="customer[last_name]"
+                    placeholder="Enter your last name"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form__field">
+                <div className="form__field__required">
+                  <label htmlFor="customer[email]">Email</label>
+                  <span className="required-field-symbol">*</span>
+                </div>
+                <p className="info muted">
+                  We send the booking confirmation to this email address
+                </p>
+                <input
+                  type="email"
+                  id="email"
+                  name="customer[email]"
+                  placeholder="Enter your email address"
+                />
+              </div>
+              <div className="form__field">
+                <label htmlFor="customer[phone]">Phone number</label>
+                <p className="info muted">
+                  We may use this to contact you about your booking
+                </p>
+                <input
+                  type="phone"
+                  id="phone"
+                  name="customer[phone]"
+                  placeholder="Enter your phone number"
+                />
+              </div>
+            </section>
             <div className="form__field">
-              <label htmlFor="customer[phone]">Phone number</label>
-              <p className="info muted">
-                We may use this to contact you about your booking
-              </p>
-              <input
-                type="phone"
-                id="phone"
-                name="customer[phone]"
-                placeholder="Enter your phone number"
-              />
+              <label htmlFor="booking[notes]">Notes</label>
+              <textarea
+                id="notes"
+                name="booking[notes]"
+                placeholder="Enter any additional notes"
+                rows={6}
+              ></textarea>
             </div>
-          </section>
-          <div className="form__field">
-            <label htmlFor="booking[notes]">Notes</label>
-            <textarea
-              id="notes"
-              name="booking[notes]"
-              placeholder="Enter any additional notes"
-              rows={6}
-            ></textarea>
           </div>
         </div>
-      </div>
 
-      <button type="submit" disabled={widgetPreview}>
-        {buttonText}
-      </button>
-    </form>
-  </section>
-);
+        <button type="submit" disabled={widgetPreview}>
+          {buttonText}
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
