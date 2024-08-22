ca2e8a4ddea2f6f03cbdae9d4b69c525221ba449
PMGH <petermccready2@gmail.com>
2024-08-22 23:07:52 +0100

    form css

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -81,167 +81,160 @@ const Form = ({
           name="widget-submission"
           value="true"
         ></input>
-        <div className="form__field__group">
-          <div className="form__field form-group-left">
-            <div className="form__field form__field__required">
-              <div className="form__field__required">
-                <label
-                  id="number_of_covers_label"
-                  htmlFor="booking[number_of_covers]"
-                >
-                  Guests{" "}
-                  {maxCoversPerBooking ? ` (Max: ${maxCoversPerBooking})` : ""}
-                </label>
-                <span className="required-field-symbol">*</span>
-              </div>
-              <input
-                type="number"
-                id="number_of_covers"
-                name="booking[number_of_covers]"
-                placeholder="1"
-                min="1"
-                value="2"
-                required
-              />
-            </div>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => date && setStartDate(date)}
-              dateFormat="dd/MM/yyyy"
-              filterDate={isPastDate}
-              showIcon
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 48 48"
-                >
-                  <mask id="ipSApplication0">
-                    <g
-                      fill="none"
-                      stroke="#fff"
-                      strokeLinejoin="round"
-                      strokeWidth="4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        d="M40.04 22v20h-32V22"
-                      ></path>
-                      <path
-                        fill="#fff"
-                        d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
-                      ></path>
-                    </g>
-                  </mask>
-                  <path
-                    fill="currentColor"
-                    d="M0 0h48v48H0z"
-                    mask="url(#ipSApplication0)"
-                  ></path>
-                </svg>
-              }
-            />
+        <div className="form-group-1">
+          <div className="form__field form__field__required">
             <div className="form__field__required">
-              <input
-                type="hidden"
-                id={datePickerHiddenInputId}
-                name="booking[date]"
-                required
-                value={formattedTodayDate}
-              ></input>
+              <label
+                id="number_of_covers_label"
+                htmlFor="booking[number_of_covers]"
+              >
+                Guests{" "}
+                {maxCoversPerBooking ? ` (Max: ${maxCoversPerBooking})` : ""}
+              </label>
+              <span className="required-field-symbol">*</span>
             </div>
-            <div className="time-select-container">
-              <Select
-                id={timePickerId}
-                name="booking[time]"
-                required
-                className="time-select"
-                options={timeOptionsAsHtml({
-                  openAt: 6,
-                  closeAt: 20,
-                })}
-              ></Select>
-            </div>
+            <input
+              type="number"
+              id="number_of_covers"
+              name="booking[number_of_covers]"
+              placeholder="1"
+              min="1"
+              value="2"
+              required
+            />
           </div>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => date && setStartDate(date)}
+            dateFormat="dd/MM/yyyy"
+            filterDate={isPastDate}
+            showIcon
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48"
+              >
+                <mask id="ipSApplication0">
+                  <g
+                    fill="none"
+                    stroke="#fff"
+                    strokeLinejoin="round"
+                    strokeWidth="4"
+                  >
+                    <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
+                    <path
+                      fill="#fff"
+                      d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
+                    ></path>
+                  </g>
+                </mask>
+                <path
+                  fill="currentColor"
+                  d="M0 0h48v48H0z"
+                  mask="url(#ipSApplication0)"
+                ></path>
+              </svg>
+            }
+          />
+          <div className="form__field__required">
+            <input
+              type="hidden"
+              id={datePickerHiddenInputId}
+              name="booking[date]"
+              required
+              value={formattedTodayDate}
+            ></input>
+          </div>
+          <Select
+            id={timePickerId}
+            name="booking[time]"
+            required
+            className="time-select"
+            options={timeOptionsAsHtml({
+              openAt: 6,
+              closeAt: 20,
+            })}
+          ></Select>
+        </div>
 
-          <div className="form-group-right">
-            <section className="customer-details">
-              <div className="form__field__group">
-                <div className="form__field">
-                  <div className="form__field__required">
-                    <label htmlFor="customer[first_name]">First name</label>
-                    <span
-                      className="required-field-symbol"
-                      id="customer_first_name_required_symbol"
-                    >
-                      *
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="customer_first_name"
-                    name="customer[first_name]"
-                    placeholder="Enter your first name"
-                    required
-                  />
-                </div>
-                <div className="form__field last-name">
-                  <div className="form__field__required">
-                    <label htmlFor="customer[last_name]">Last name</label>
-                    <span
-                      className="required-field-symbol"
-                      id="customer_last_name_required_symbol"
-                    >
-                      *
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="customer_last_name"
-                    name="customer[last_name]"
-                    placeholder="Enter your last name"
-                    required
-                  />
-                </div>
-              </div>
+        <div className="form-group-2">
+          <section className="customer-details">
+            <div className="form__field__group">
               <div className="form__field">
                 <div className="form__field__required">
-                  <label htmlFor="customer[email]">Email</label>
-                  <span className="required-field-symbol">*</span>
+                  <label htmlFor="customer[first_name]">First name</label>
+                  <span
+                    className="required-field-symbol"
+                    id="customer_first_name_required_symbol"
+                  >
+                    *
+                  </span>
                 </div>
-                <p className="info muted">
-                  We send the booking confirmation to this email address
-                </p>
                 <input
-                  type="email"
-                  id="email"
-                  name="customer[email]"
-                  placeholder="Enter your email address"
+                  type="text"
+                  id="customer_first_name"
+                  name="customer[first_name]"
+                  placeholder="Enter your first name"
+                  required
                 />
               </div>
-              <div className="form__field">
-                <label htmlFor="customer[phone]">Phone number</label>
-                <p className="info muted">
-                  We may use this to contact you about your booking
-                </p>
+              <div className="form__field last-name">
+                <div className="form__field__required">
+                  <label htmlFor="customer[last_name]">Last name</label>
+                  <span
+                    className="required-field-symbol"
+                    id="customer_last_name_required_symbol"
+                  >
+                    *
+                  </span>
+                </div>
                 <input
-                  type="phone"
-                  id="phone"
-                  name="customer[phone]"
-                  placeholder="Enter your phone number"
+                  type="text"
+                  id="customer_last_name"
+                  name="customer[last_name]"
+                  placeholder="Enter your last name"
+                  required
                 />
               </div>
-            </section>
-            <div className="form__field">
-              <label htmlFor="booking[notes]">Notes</label>
-              <textarea
-                id="notes"
-                name="booking[notes]"
-                placeholder="Enter any additional notes"
-                rows={6}
-              ></textarea>
             </div>
+            <div className="form__field">
+              <div className="form__field__required">
+                <label htmlFor="customer[email]">Email</label>
+                <span className="required-field-symbol">*</span>
+              </div>
+              <p className="info muted">
+                We send the booking confirmation to this email address
+              </p>
+              <input
+                type="email"
+                id="email"
+                name="customer[email]"
+                placeholder="Enter your email address"
+              />
+            </div>
+            <div className="form__field">
+              <label htmlFor="customer[phone]">Phone number</label>
+              <p className="info muted">
+                We may use this to contact you about your booking
+              </p>
+              <input
+                type="phone"
+                id="phone"
+                name="customer[phone]"
+                placeholder="Enter your phone number"
+              />
+            </div>
+          </section>
+          <div className="form__field">
+            <label htmlFor="booking[notes]">Notes</label>
+            <textarea
+              id="notes"
+              name="booking[notes]"
+              placeholder="Enter any additional notes"
+              rows={6}
+            ></textarea>
           </div>
         </div>
 
