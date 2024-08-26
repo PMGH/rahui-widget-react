a80409856a72716a848d6fa131c653cb73b029cd
PMGH <petermccready2@gmail.com>
2024-08-26 11:15:29 +0100

    update PoweredBy href

--- src/components/powered-by/index.tsx
+++ src/components/powered-by/index.tsx
@@ -4,7 +4,11 @@ const PoweredBy = () => (
   <div className="powered-by">
     <p className="muted">
       Powered by{" "}
-      <a href="rahui-booking.com" target="_blank" className="muted styled-link">
+      <a
+        href="https://www.rahui-booking.com"
+        target="_blank"
+        className="muted styled-link"
+      >
         rahui-booking.com
       </a>
     </p>
