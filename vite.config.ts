3a79a5802ae24667cd0148c915dddd5b559c31d5
PMGH <petermccready2@gmail.com>
2024-08-26 16:14:51 +0100

    setup husky and build bundle.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx",
+      output: {
+        format: "iife",
+        dir: "dist",
+        entryFileNames: "bundle.js",
+      },
+    },
+  },
+});
